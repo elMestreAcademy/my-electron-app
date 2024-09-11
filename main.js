1af30ef9f6b908fe86dc340b68a2f2b8007bec9b
Carlos Mestre <carmesce@gmail.com>
2024-09-11 12:04:13 +0200

    Electron Hello World tutorial

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+
+// Sale de la aplicación al cerrar la ventana 
+// (Ahora es necesario en macOS)
+app.on('window-all-closed', () => {
+  // if (process.platform !== 'darwin') {
+    app.quit()
+  // }
+})
